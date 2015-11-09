97d959976ebd13890c6a5e754ba48d0e2ae4a02a
Jared Hensley <jahensle85@me.com>
2015-11-08 22:34:02 -0600

    changes

--- service.js
+++ service.js
@@ -8,12 +8,6 @@ angular.module('myApp').service('mainService', function ($http) {
       .then(function (response) {
         console.log(response);
         var slimResults = (response.data.data);
-        /*console.log(slimResults);*/
-        /*var imageURLS = [];*/
-        /*  slimResults.forEach(function (obj) {
-            imageURLS.push(obj.images['standard_resolution'].url);
-          });*/
-        /*return imageURLS;*/
         return slimResults;
       }, function (err) {
         console.log('error');
