2c38fe86890f6ed2503c47837f73078e3f1d2b0a
Jared Hensley <jahensle85@me.com>
2015-11-08 22:18:37 -0600

    basic styles

--- service.js
+++ service.js
@@ -6,12 +6,15 @@ angular.module('myApp').service('mainService', function ($http) {
         url: 'https://api.instagram.com/v1/tags/' + searchTerm + '/media/recent?client_id=ab31057e67224170ad5709c5293b17c6&callback=JSON_CALLBACK'
       })
       .then(function (response) {
+        console.log(response);
         var slimResults = (response.data.data);
-        var imageURLS = [];
-        slimResults.forEach(function (obj) {
-          imageURLS.push(obj.images['standard_resolution'].url);
-        });
-        return imageURLS;
+        /*console.log(slimResults);*/
+        /*var imageURLS = [];*/
+        /*  slimResults.forEach(function (obj) {
+            imageURLS.push(obj.images['standard_resolution'].url);
+          });*/
+        /*return imageURLS;*/
+        return slimResults;
       }, function (err) {
         console.log('error');
       });
