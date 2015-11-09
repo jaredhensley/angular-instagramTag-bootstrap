cb4d2007efd368eeeb7f01c4b3ec917738eea37b
Jared Hensley <jahensle85@me.com>
2015-11-08 22:39:48 -0600

    empty input after submit

--- controller.js
+++ controller.js
@@ -1,9 +1,9 @@
 angular.module('myApp').controller('mainController', function ($scope, mainService) {
-  $scope.test = 'TEST';
 
   $scope.getResults = function (searchTerm) {
     mainService.getData(searchTerm).then(function (res) {
       $scope.results = res;
+      $scope.searchTerm = '';
     });
   };
 
